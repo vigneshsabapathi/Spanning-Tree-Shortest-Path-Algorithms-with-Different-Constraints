a75b5b258ec08a4773db761751ca82d6e9847f18
Vignesh <vignesh.sabapathi@gmail.com>
2026-03-26 08:28:14 +0530

    Add union-find test suite

---
+++ tests/test_union_find.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "union_find.h"
+
+#define ASSERT(cond, msg) do { if (!(cond)) { fprintf(stderr, "FAIL [%s:%d]: %s\n", __FILE__, __LINE__, msg); exit(1); } } while(0)
+#define PASS(msg) printf("PASS: %s\n", msg)
+
+int main(void) {
+    /* Create UF of size 5, verify find(i)==i for all i */
+    UnionFind *uf = uf_create(5);
+    ASSERT(uf != NULL, "uf_create returned NULL");
+    for (int i = 0; i < 5; i++) {
+        ASSERT(uf_find(uf, i) == i, "initial find(i) should equal i");
+    }
+    PASS("initial find(i)==i for all i");
+
+    /* Union(0,1): verify connected(0,1)==true, connected(0,2)==false */
+    uf_union(uf, 0, 1);
+    ASSERT(uf_connected(uf, 0, 1) == true,  "connected(0,1) should be true after union");
+    ASSERT(uf_connected(uf, 0, 2) == false, "connected(0,2) should be false");
+    PASS("union(0,1): connected(0,1)==true, connected(0,2)==false");
+
+    /* Union(2,3): verify connected(2,3)==true */
+    uf_union(uf, 2, 3);
+    ASSERT(uf_connected(uf, 2, 3) == true, "connected(2,3) should be true after union");
+    PASS("union(2,3): connected(2,3)==true");
+
+    /* Union(1,3): verify all of 0,1,2,3 are connected, 4 is not */
+    uf_union(uf, 1, 3);
+    ASSERT(uf_connected(uf, 0, 1) == true, "connected(0,1) should be true");
+    ASSERT(uf_connected(uf, 0, 2) == true, "connected(0,2) should be true after merging components");
+    ASSERT(uf_connected(uf, 0, 3) == true, "connected(0,3) should be true");
+    ASSERT(uf_connected(uf, 1, 2) == true, "connected(1,2) should be true");
+    ASSERT(uf_connected(uf, 1, 3) == true, "connected(1,3) should be true");
+    ASSERT(uf_connected(uf, 2, 3) == true, "connected(2,3) should be true");
+    ASSERT(uf_connected(uf, 0, 4) == false, "connected(0,4) should be false");
+    ASSERT(uf_connected(uf, 1, 4) == false, "connected(1,4) should be false");
+    ASSERT(uf_connected(uf, 2, 4) == false, "connected(2,4) should be false");
+    ASSERT(uf_connected(uf, 3, 4) == false, "connected(3,4) should be false");
+    PASS("union(1,3): all of 0,1,2,3 connected, none connected to 4");
+
+    /* Test reset: after reset, verify find(i)==i again */
+    uf_reset(uf);
+    for (int i = 0; i < 5; i++) {
+        ASSERT(uf_find(uf, i) == i, "after reset, find(i) should equal i");
+    }
+    PASS("reset: find(i)==i for all i");
+
+    /* Destroy */
+    uf_destroy(uf);
+    PASS("uf_destroy completed");
+
+    return 0;
+}
