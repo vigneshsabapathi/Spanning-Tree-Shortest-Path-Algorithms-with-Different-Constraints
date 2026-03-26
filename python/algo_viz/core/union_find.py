f113f4044ad5cea29a09356372b07960bf9f1cb3
Vignesh <vignesh.sabapathi@gmail.com>
2026-03-26 11:04:07 +0530

    Remove obvious comments in union_find.py

--- python/algo_viz/core/union_find.py
+++ python/algo_viz/core/union_find.py
@@ -5,11 +5,9 @@ class UnionFind:
         self.rank: list[int] = [0] * size
 
     def find(self, x: int) -> int:
-        # First pass: walk to root
         root = x
         while self.parent[root] != root:
             root = self.parent[root]
-        # Second pass: flatten (path compression)
         while self.parent[x] != root:
             self.parent[x], x = root, self.parent[x]
         return root
@@ -18,7 +16,6 @@ class UnionFind:
         rx, ry = self.find(x), self.find(y)
         if rx == ry:
             return False
-        # Union by rank
         if self.rank[rx] < self.rank[ry]:
             rx, ry = ry, rx
         self.parent[ry] = rx
